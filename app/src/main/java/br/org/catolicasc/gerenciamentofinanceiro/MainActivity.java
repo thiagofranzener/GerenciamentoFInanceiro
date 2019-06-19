67d2bdfabbc7a55f66dab511d52b0588900fb3e0
Jonathan <jonathanwitkosky@gmail.com>
2019-06-18 21:55:27 -0300

    Mudança da main activity e desenvolvimento das telas.

--- app/src/main/java/br/org/catolicasc/gerenciamentofinanceiro/MainActivity.java
+++ app/src/main/java/br/org/catolicasc/gerenciamentofinanceiro/MainActivity.java
@@ -2,95 +2,34 @@ package br.org.catolicasc.gerenciamentofinanceiro;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-
-import java.util.ArrayList;
+import android.view.View;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        ViewPager viewPager = findViewById(R.id.view_pager);
-
+        setContentView(R.layout.main_activity);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle(R.string.app_name);
 
-        TabLayout tabs = findViewById(R.id.tabs);
+        FloatingActionButton fab = findViewById(R.id.btnAddUsuario);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mIntent = new Intent(view.getContext(), CadastroUsuario.class);
+                startActivityForResult(mIntent, 0);
+            }
+        });
 
+        mListView = findViewById(R.id.listview);
 
-        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
-
-        viewPagerAdapter.addFragment(new Saldo(), "Saldo");
-        viewPager.setAdapter(viewPagerAdapter);
-        tabs.setupWithViewPager(viewPager);
-
-        viewPagerAdapter.addFragment(new Grafico(), "Gráfico de despesas");
-        viewPager.setAdapter(viewPagerAdapter);
-        tabs.setupWithViewPager(viewPager);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.addReceita:
-                startActivity(new Intent(MainActivity.this, AdicionarReceita.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
-                return true;
-            case R.id.addDespesa:
-                startActivity(new Intent(MainActivity.this, AdicionarDespesa.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
-                return true;
-        }
-        return false;
-    }
-
-    class ViewPagerAdapter extends FragmentPagerAdapter {
-
-        private ArrayList<Fragment> fragments;
-        private ArrayList<String> titles;
-
-        ViewPagerAdapter(FragmentManager fm){
-            super(fm);
-            this.fragments = new ArrayList<>();
-            this.titles = new ArrayList<>();
-        }
-
-        @Override
-        public Fragment getItem(int i) {
-            return fragments.get(i);
-        }
-
-        @Override
-        public int getCount() {
-            return fragments.size();
-        }
-
-        public void addFragment(Fragment fragment, String title){
-            fragments.add(fragment);
-            titles.add(title);
-        }
-
-        @Nullable
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return titles.get(position);
-        }
-    }
 }
